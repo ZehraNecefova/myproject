8c22024b9bdf1f5fe309ad4b1b69da1e996a8f8d
Zehra Necefova <zehranecefova2306@gmail.com>
2025-08-13 22:38:00 +0400

    fix errors

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,6 +1,14 @@
-def main():
-    print("Load model and run predictions")
+import pandas as pd
+import joblib
 
+def main():
+    model = joblib.load("models/best_model.pkl")
+    
+    df_new = pd.read_parquet("multisim_dataset.parquet")  
+    
+    predictions = model.predict(df_new)
+    print("Predictions:")
+    print(predictions)
 
 if __name__ == "__main__":
     main()
